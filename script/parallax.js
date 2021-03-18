485366265d4f0d5ad3bfc4c0b9094cd2025b37d5
jackgraddon <jjg0704@icloud.com>
2021-03-18 16:36:23 -0700

    Testing
    Testing new parallax effects with the body of the main page, as well as added classes for image shadows.

--- script/parallax.js
+++ script/parallax.js
@@ -1,28 +1,34 @@
 const backgrounds = document.querySelectorAll("#background");
+const header = document.querySelectorAll("main");
 const range = 20;
 
-const calcValue = (a, b) => (a / b * range - range / 2).toFixed(1);
+const calcValue = (a, b) => ((a / b) * range - range / 2).toFixed(1);
 
 let timeout;
-document.addEventListener('mousemove', ({
-    x,
-    y
-}) => {
+document.addEventListener(
+  "mousemove",
+  ({ x, y }) => {
     if (timeout) {
-        window.cancelAnimationFrame(timeout);
+      window.cancelAnimationFrame(timeout);
     }
 
     timeout = window.requestAnimationFrame(() => {
-        const yValue = calcValue(y, window.innerHeight);
-        const xValue = calcValue(x, window.innerWidth);
+      const yValue = calcValue(y, window.innerHeight);
+      const xValue = calcValue(x, window.innerWidth);
 
-        // [].forEach.call(header, (header) => {
-        //     // header.style.transform = `translateX(${-xValue * 1.2}px) translateY(${-yValue * 1.2}px)`;
-        //     header.style.transform = `translateX(0) translateY(0)`;
-        // });
+      [].forEach.call(header, (header) => {
+        header.style.transform = `translateX(${xValue * 0.05}vw) translateY(${
+          yValue * 0.05
+        }vh)`;
+        // header.style.transform = `translateX(0) translateY(0)`;
+      });
 
-        [].forEach.call(backgrounds, (background) => {
-            background.style.backgroundPosition = `${-xValue * .45}px ${-yValue * .45}px`;
-        });
+      [].forEach.call(backgrounds, (background) => {
+        background.style.backgroundPosition = `${-xValue * 0.1 - 5}vw ${
+          -yValue * 0.1 - 5
+        }vh`;
+      });
     });
-}, false);
+  },
+  false
+);
