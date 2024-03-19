8330e864114af15b179638c9c334517f3051ab39
jackgraddon <hello@jackgraddon.com>
2024-03-19 09:54:36 -0700

    Fixing cloud images

--- js/jg-theme.js
+++ js/jg-theme.js
@@ -1,23 +1,22 @@
-// Gets local time and sets the theme and greeting accordingly
-
+// Defines the time windows for each theme style
 const times = {
-    morning: {
-        start: 5,
-        end: 12
-    },
-    afternoon: {
-        start: 12,
-        end: 18
-    },
-    evening: {
-        start: 18,
-        end: 22
-    },
-    night: {
-        start: 22,
-        end: 5
-    }
-}
+  morning: {
+    start: 5,
+    end: 12,
+  },
+  afternoon: {
+    start: 12,
+    end: 18,
+  },
+  evening: {
+    start: 18,
+    end: 22,
+  },
+  night: {
+    start: 22,
+    end: 5,
+  },
+};
 
 // Get the current time
 var currentTime = new Date().getHours();
@@ -27,42 +26,54 @@ var currentTime = new Date().getHours();
 
 // Set the theme based on the time
 function setTimeTheme() {
-    // Morning theme: 5am to 12pm
-    if (currentTime >= times.morning.start && currentTime < times.morning.end) {
-        document.body.dataset.jgTheme = "morning";
-        console.log("morning");
-    }
+  // Randomly pick a cloud image for the divider
+  var cloudImage = Math.floor(Math.random() * 3) + 1;
+  document.querySelector("img#cloud").src =
+    `https://jackgraddon.com/images/png/front/cloud${cloudImage}.png`;
 
-    // Afternoon theme: 12pm to 6pm
-    if (currentTime >= times.afternoon.start && currentTime < times.afternoon.end) {
-        document.body.dataset.jgTheme = "afternoon";
-        console.log("afternoon");
-    }
+  // Morning theme: 5am to 12pm
+  if (currentTime >= times.morning.start && currentTime < times.morning.end) {
+    document.body.dataset.jgTheme = "morning";
+    console.log("morning");
+  }
 
-    // Evening theme: 6pm to 10pm
-    if (currentTime >= times.evening.start && currentTime < times.evening.end) {
-        document.body.dataset.jgTheme = "evening";
-        console.log("evening");
-    }
+  // Afternoon theme: 12pm to 6pm
+  if (
+    currentTime >= times.afternoon.start &&
+    currentTime < times.afternoon.end
+  ) {
+    document.body.dataset.jgTheme = "afternoon";
+    console.log("afternoon");
+  }
 
-    // Night theme: 10pm to 5am
-    if (currentTime >= times.night.start || currentTime < times.night.end) {
-        document.body.dataset.jgTheme = "night";
-        console.log("night");
-    }
+  // Evening theme: 6pm to 10pm
+  if (currentTime >= times.evening.start && currentTime < times.evening.end) {
+    document.body.dataset.jgTheme = "evening";
+    console.log("evening");
+  }
 
-    // If the browser's theme is dark, set the theme to dark
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.body.dataset.jgTheme = document.body.dataset.jgTheme + "-dark";
-        console.log("dark");
-    }
+  // Night theme: 10pm to 5am
+  if (currentTime >= times.night.start || currentTime < times.night.end) {
+    document.body.dataset.jgTheme = "night";
+    console.log("night");
+  }
+
+  // If the browser's theme is dark, set the theme to dark
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    document.body.dataset.jgTheme = document.body.dataset.jgTheme + "-dark";
+    console.log("dark");
+  }
 }
 
 // Set the theme
 setTimeTheme();
 
 // Watch for changes to the browser's theme and update the theme accordingly
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (e) => {
     setTimeTheme();
-});
-
+  });
