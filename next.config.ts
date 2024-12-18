e730c429c1fa733d0e911ba1c5d90026acdcfc28
Jack Graddon <hello@jackgraddon.com>
2024-12-18 14:22:41 -0800

    Removing basePath configuration, it seems to be breaking the deployment.

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-    basePath: process.env.NODE_ENV === 'production' ? '/website-beta' : '/',
     images: {
       unoptimized: true,
     },
     env: {
       NEXT_PUBLIC_APP_NAME: "Jack Graddon's Beta Website",
-    }
+    },
+    module: {
+        extends: ['nextjs'],
+    },
 };
 
 export default nextConfig;
