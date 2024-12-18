45d1927f153200bb671e3431285eb9734dc6ec5e
Jack Graddon <hello@jackgraddon.com>
2024-12-18 14:11:21 -0800

    Configuring for GitHub pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+    basePath: process.env.NODE_ENV === 'production' ? '/website-beta' : '/',
+    images: {
+      unoptimized: true,
+    },
+    env: {
+      NEXT_PUBLIC_APP_NAME: "Jack Graddon's Beta Website",
+    }
 };
 
 export default nextConfig;
