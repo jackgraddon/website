da2c64fd93f2e75fee839c5d078223bc64416735
Jack Graddon <hello@jackgraddon.com>
2024-12-24 12:19:55 -0800

    A few updates to packages

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    env: {
-      NEXT_PUBLIC_APP_NAME: "Jack Graddon's Beta Website",
-    },
-    module: {
-        extends: ['nextjs'],
-    },
+  images: { unoptimized: true },
+  sassOptions: {
+    additionalData: `
+      $mobile-width: 480px
+      $tablet-width: 768px
+      $desktop-width: 1024px`
+  },
+  env: {
+    NEXT_PUBLIC_APP_NAME: "Jack Graddon's Beta Website",
+  },
+  module: {
+      extends: ['nextjs'],
+  },
 };
 
 export default nextConfig;
