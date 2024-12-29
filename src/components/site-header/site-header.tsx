85b069ea67c3f3684194424129cbb0e950c09a2e
Jack Graddon <hello@jackgraddon.com>
2024-12-29 09:26:09 -0800

    Added a header (nothing in there yet, though)

---
+++ src/components/site-header/site-header.tsx
@@ -0,0 +1,9 @@
+import styles from './site-header.module.sass';
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <p>© 2024 Jackson Graddon. All rights reserved.</p>
+    </div>
+  );
+};
