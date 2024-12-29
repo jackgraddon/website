73e2eca7500f56561c1941ca89c42f3399f970a2
Jack Graddon <hello@jackgraddon.com>
2024-12-29 12:32:25 -0800

    Added a footer

--- src/components/site-footer/site-footer.tsx
+++ src/components/site-footer/site-footer.tsx
@@ -1,9 +1,34 @@
-import styles from './site-footer.module.sass';
+import Link from 'next/link';
+import styles from '@/components/site-footer/site-footer.module.sass';
 
 export default function Footer() {
     return (
-        <div className={styles.footer}>
-           <p>© 2024 Jackson Graddon. All rights reserved.</p>
+      <div className={styles.footer}>
+        <div className={styles.footerCauses}>
+          <a id="data-rights" href="https://epic.org" title={"Learn about your Data Rights"} target="_blank">&#x1F512;</a>
+          <a id="help-me" href="https://ko-fi.com/jackgraddon" title={"Buy me a Coffee"} target="_blank">&#x1FA99;</a>
         </div>
+        <h3>Jack Graddon</h3>
+        <p>Website © Jack Graddon 2024</p>
+        <div>
+          <ul className={styles.nav}>
+            <li className={styles.navItem}>
+              <Link href="/">Home</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/about/">About</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/projects/">Projects</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/contact/">Contact</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/legal/">Legal</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     );
 };
