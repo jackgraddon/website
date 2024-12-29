8454f887af59ec0669388b3186b9143df5e37509
Jack Graddon <hello@jackgraddon.com>
2024-12-28 23:09:00 -0800

    Updated Project Deck

--- src/components/project-deck/deck.tsx
+++ src/components/project-deck/deck.tsx
@@ -1,15 +1,18 @@
 import '@/styles/globals.sass'
 import styles from '@/components/project-deck/deck.module.sass'
+import Image from "next/image";
+import ImageDTC from '@/images/dtc.jpg'
+import ImageFBLA from '@/images/fblaProject22.jpg'
+import ImageIG from '@/Images/instagram.jpg'
 
 export default function ProjectDeck() {
   return (
     <div className={styles.deck}>
       <div className={styles.tile}
            style={{
-             backgroundImage: `url('./projects/web/fbla/2022/preview.jpg')`,
              backgroundColor: 'rgb(67, 37, 52)',
-           }}
-           aria-label="Future Business Leaders of America 2021/22 Competition - Website Design.">
+           }}>
+        <Image src={ImageFBLA} alt={"Future Business Leaders of America 2021/22 Competition - Website Design."}></Image>
         <a className="btn"
            // onClick={() => openModal('project', '0')}
            style={{color: 'rgb(196, 73, 0)'}}
@@ -17,10 +20,9 @@ export default function ProjectDeck() {
       </div>
       <div className={styles.tile}
            style={{
-             backgroundImage: `url('./projects/design/instagram/preview.jpg')`,
              backgroundColor: 'rgb(200, 55, 171)',
-           }}
-           aria-label="Instagram logo with the word 'posts' on its right.">
+           }}>
+        <Image src={ImageIG} alt={"Instagram logo with the word 'posts' on its right."}></Image>
         <a className="btn"
            // onClick={() => openModal('project', '1')}
            style={{color: 'rgb(55, 113, 200)'}}
@@ -28,10 +30,9 @@ export default function ProjectDeck() {
       </div>
       <div className={styles.tile}
            style={{
-             backgroundImage: `url('./projects/design/dtc/preview.jpg')`,
              backgroundColor: 'rgb(166, 15, 45)'
-           }}
-           aria-label="WSU Logo with the letters DTC, which stands for Digital Technology and Culture.">
+           }}>
+        <Image src={ImageDTC} alt={"WSU Logo with the letters DTC, which stands for Digital Technology and Culture."}></Image>
         <a className="btn"
            // onClick={() => openModal('project', '2')}
            style={{color: 'rgb(77, 77, 77)'}}
