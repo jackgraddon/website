f8621074032bc997bf0b3f85236fb4d01fe3b3b3
Jack Graddon <hello@jackgraddon.com>
2024-12-30 11:28:11 -0800

    Added Splash fallback

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,11 +1,12 @@
 import { Suspense } from "react";
+import Splash from "@/components/splash/splash";
 import ProjectPage from "./ProjectPage"; // Import Client Component
 
 export const dynamic = "force-dynamic"; // Dynamically render this page
 
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading project...</div>}>
+    <Suspense fallback={<Splash title={"Loading..."} subtitle={"Please hang tight!"}></Splash>}>
       <ProjectPage />
     </Suspense>
   );
