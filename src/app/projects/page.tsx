e16f8c242ec885c8a8b82f9f977402aa04b6bfa9
Jack Graddon <hello@jackgraddon.com>
2024-12-29 23:28:04 -0800

    Added the projects page, which will display three curated projects, and a list of the rest from newest to oldest. I will make the timeline prettier eventually.

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import ProjectDeck from "@/components/project-deck/deck";
+import Splash from "@/components/splash/splash";
+import { projectDictionary } from "@/utils/project";
+
+export default function ProjectsPage() {
+  const topProjects = ["0", "0", "0"]
+
+  // Convert projectDictionary to an array of projects and sort them by ID (descending)
+  const timelineProjects = Object.keys(projectDictionary)
+    .filter(
+      (id) => !topProjects.some((project) => project.id === id) // Exclude top projects
+    )
+    .sort((a, b) => parseInt(b) - parseInt(a)) // Sort by ID in descending order
+    .map((id) => ({
+      id,
+      title: `Project ${id}`, // Default title, replace if you have a better mapping
+    }));
+
+  return (
+    <div>
+      <Splash title={"Projects"} subtitle={"My Portfolio"}></Splash>
+      <main>
+        <h2>Top Projects</h2>
+        <ProjectDeck firstId={topProjects[0]} secondId={topProjects[1]} thirdId={topProjects[2]}></ProjectDeck>
+        <h2>All Projects</h2>
+        <ul>
+          {timelineProjects.map((project) => (
+            <li key={project.id}>
+              <Link href={`/project?id=${project.id}`}>{project.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  )
+}
