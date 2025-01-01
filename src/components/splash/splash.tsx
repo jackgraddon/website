50784ddeda63419e816d05f337a15c9d285a1c75
Jack Graddon <hello@jackgraddon.com>
2025-01-01 10:24:03 -0800

    Fixed splash animation to a set amount of pixels instead of entire screen or targeted elements (they didn't work).

--- src/components/splash/splash.tsx
+++ src/components/splash/splash.tsx
@@ -6,10 +6,10 @@
 
 "use client"; // This marks the component as a client-side component
 import Image from "next/image";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { easeInOut } from "motion";
+import { motion, useScroll, useTransform } from "motion/react";
 import styles from "@/components/splash/splash.module.sass";
 import SplashCloud from "@/components/splash/splash-cloud.webp";
-import {useRef} from "react";
 
 /**
  * Interface representing the properties for the Splash component.
@@ -33,43 +33,36 @@ interface SplashProps {
  * @returns {JSX.Element} The rendered splash screen component.
  */
 const Splash = ({ title, subtitle }: SplashProps) => {
-  // Track the scroll progress
-  const containerRef = useRef(null);
-  const targetRef = useRef(null);
+  const { scrollY } = useScroll();
+  const yRange = useTransform(scrollY, [0, 500], [0, 1], { ease: easeInOut });
+  const opacity = useTransform(yRange, [0, 1], [1, 0], { ease: easeInOut });
+  const filter = useTransform(yRange, [0, 1], ["blur(0px)", "blur(10px)"], { ease: easeInOut });
+  const scale = useTransform(yRange, [0, 1], [1, 0.9], { ease: easeInOut });
+  const y = useTransform(yRange, [0, 1], [-50, -100], { ease: easeInOut });
 
-  const { scrollYProgress } = useScroll({
-    // container: containerRef,
-    // target: targetRef,
-    offset: ['start start', 'center start'],
-
-  });
-
-  // useMotionValueEvent(scrollYProgress, "change", (latest) => {
-  //   console.log("Page scroll: ", latest)
-  // })
-
-  // Use transforms with scrollYProgress ranging from 0 to 1
-  const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
-  const blur = useTransform(scrollYProgress, [0, 1], ['blur(0px)', 'blur(10px)']);
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.9]);
-  const y = useTransform(scrollYProgress, [0, 1], [0, -500]);
-
+  const transform = useTransform(
+    [y, scale],
+    ([latestY, latestScale]) => `translate(-50%, ${latestY}%) scale(${latestScale})`
+  )
 
   return (
-  <div className={styles.splash} ref={containerRef}>
-    <motion.div
-      className={styles.splashTitle}
-      initial={{transform: 'translate(-50%, -50%) scale(1)'}}
-      style={{opacity, filter: blur, scale, y}}
-    >
-      <h1 className={styles.title}>{title}</h1>
-      <p className={styles.subtitle}>{subtitle}</p>
-    </motion.div>
-
-    <Image src={SplashCloud.src} width={1043} height={607} alt="A cloud, covering the width of the screen"
-           className={styles.splashCloud} ref={targetRef} priority={true}/>
-  </div>
-  )
-}
+    <div className={styles.splash}>
+      <motion.div className={styles.splashTitle}
+                  initial={{ opacity: 1, filter: "blur(0px)", transform: "translate(-50%, -50%) scale(1)" }}
+                  style={{ opacity, filter, transform}}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.subtitle}>{subtitle}</p>
+      </motion.div>
+      <Image
+        src={SplashCloud.src}
+        width={1043}
+        height={607}
+        alt="A cloud, covering the width of the screen"
+        className={styles.splashCloud}
+        priority={true}
+      />
+    </div>
+  );
+};
 
 export default Splash;
