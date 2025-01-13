07d447a5accf0c04aecfc6659f19058316188ee5
Jack Graddon <hello@jackgraddon.com>
2025-01-12 18:54:27 -0800

    Wrapping in Suspense correctly this time, added some documentation.

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,6 +1,8 @@
+// noinspection GrazieInspection
+
 /**
  * Name: Project Page
- * Description: Display a project page with its title and content. Pulls data from GitHub from info looked up in project dictionary. Uses ID passed in URL.
+ * Description: A page that displays the README of a project from GitHub. The project ID is passed as a query parameter.
  * Author: Jack Graddon
  */
 
@@ -9,7 +11,7 @@
 import "@/styles/globals.sass";
 import Link from "next/link";
 import ReactMarkdown, {ExtraProps} from "react-markdown";
-import remarkGfm from 'remark-gfm'
+import remarkGfm from 'remark-gfm';
 import { useSearchParams } from "next/navigation";
 import { projectDictionary } from "@/utils/project";
 import { useEffect, useState, Suspense } from "react";
@@ -20,6 +22,11 @@ import Image from "next/image";
 
 const BASE_URL = "https://raw.githubusercontent.com/jackgraddon/";
 
+/**
+ * Removes the Table of Contents section from the given content.
+ * @param {string} content - The content from which to remove the Table of Contents.
+ * @returns {string} - The content without the Table of Contents section.
+ */
 function removeTableOfContents(content: string): string {
   const tocRegex = /## Table of Contents[\s\S]*?(?=##|$)/i;
   return content.replace(tocRegex, '');
@@ -27,56 +34,52 @@ function removeTableOfContents(content: string): string {
 
 export const dynamic = "force-dynamic"; // Dynamically render this page
 
-export default function ProjectPage() {
-  const searchParams = useSearchParams();
-  const id = searchParams.get("id");
+/**
+ * Component to fetch and display project content based on the project ID.
+ * @param {Object} props - The component props.
+ * @param {string} props.id - The project ID.
+ * @returns {JSX.Element} - The rendered project content.
+ */
+function ProjectContent({ id }: { id: string }) {
   const [title, setTitle] = useState<string | null>(null);
   const [description, setDescription] = useState<string | null>(null);
   const [repoUrl, setUrl] = useState<string | null>(null);
   const [readmeContent, setReadmeContent] = useState<string | null>(null);
 
   useEffect(() => {
-    // Check if 'id' is defined and if it exists in the 'projectDictionary'
     if (id && projectDictionary[id]) {
-      // Construct the full URL to the 'details.json' file
       const detailsUrl = `${BASE_URL}${projectDictionary[id].url}`;
 
-      // Fetch the details.json file associated with the project ID
       fetch(detailsUrl)
         .then((response) => {
-          // Check if the response is not OK (status code outside the range 200-299)
           if (!response.ok) {
             throw new Error("Failed to fetch project details");
           }
-          return response.json(); // Parse the response body as JSON
+          return response.json();
         })
         .then((projectDetails) => {
-          // Update the state with data from 'details.json'
           setTitle(projectDetails.name);
           setDescription(projectDetails.description);
           setUrl(`https://github.com/jackgraddon/${projectDetails.url.replace('/main/README.md', '')}`);
 
-          // Fetch the README file from the URL specified in 'details.json'
           const readmeUrl = `${BASE_URL}${projectDetails.url}`;
           return fetch(readmeUrl);
         })
         .then((response) => {
-          // Check if the response is not OK
           if (!response.ok) {
             throw new Error("Failed to fetch README");
           }
-          return response.text(); // Parse the response body as text
+          return response.text();
         })
-        .then((readmeContent) => setReadmeContent(removeTableOfContents(readmeContent))) // Set the fetched README content to state
+        .then((readmeContent) => setReadmeContent(removeTableOfContents(readmeContent)))
         .catch((error) => {
-          console.error(error); // Log any errors to the console
-          setReadmeContent("Failed to load project README."); // Update state with an error message
+          console.error(error);
+          setReadmeContent("Failed to load project README.");
         });
     } else {
-      // If 'id' is not valid or does not exist in 'projectDictionary', set an error message
       setReadmeContent("Invalid project ID.");
     }
-  }, [id]); // Dependency array: effect runs when 'id' changes
+  }, [id]);
 
   useEffect(() => {
     if (title) {
@@ -96,7 +99,7 @@ export default function ProjectPage() {
   }, [title, description]);
 
   const CustomImage = ({ alt, src, width, height, ...props }: { alt: string; src: string; width?: number | `${number}`; height?: number | `${number}` } & React.ImgHTMLAttributes<HTMLImageElement>) => (
-      <Image alt={alt} src={src} width={width} height={height} style={{ maxWidth: '100%' }} {...props} />
+    <Image alt={alt} src={src} width={width} height={height} style={{ maxWidth: '100%' }} {...props} />
   );
 
   const renderers = {
@@ -104,28 +107,42 @@ export default function ProjectPage() {
   };
 
   return (
+    <div>
+      <Splash title={title ? title : "Loading..."} subtitle={description ? description : "Please hang tight!"}></Splash>
+      <main style={{textAlign: 'center'}}>
+        <section>
+          {readmeContent ? (
+            <ReactMarkdown remarkPlugins={[remarkGfm]} components={renderers}>
+              {readmeContent}
+            </ReactMarkdown>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </section>
+        <section>
+          {repoUrl ? (
+            <Link href={repoUrl!} className={"btn"} target={'_blank'} rel={'noopener noreferrer'}>View Repository</Link>
+          ) : (
+            <p>No repository URL provided.</p>
+          )}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+/**
+ * Main component for the project page.
+ * Fetches the project ID from the URL and renders the project content.
+ * @returns {JSX.Element} - The rendered project page.
+ */
+export default function ProjectPage() {
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+
+  return (
     <Suspense fallback={<Splash title={"Loading..."} subtitle={"Please hang tight!"}></Splash>}>
-      <div>
-        <Splash title={title ? title : "Loading..."} subtitle={description ? description : "Please hang tight!"}></Splash>
-        <main style={{textAlign: 'center'}}>
-          <section>
-            {readmeContent ? (
-                <ReactMarkdown remarkPlugins={[remarkGfm]} components={renderers}>
-                  {readmeContent}
-                </ReactMarkdown>
-            ) : (
-                <p>Loading...</p>
-            )}
-          </section>
-          <section>
-            {repoUrl ? (
-                <Link href={repoUrl!} className={"btn"} target={'_blank'} rel={'noopener noreferrer'}>View Repository</Link>
-            ) : (
-                <p>No repository URL provided.</p>
-            )}
-          </section>
-        </main>
-      </div>
+      {id ? <ProjectContent id={id} /> : <p>Invalid project ID.</p>}
     </Suspense>
   );
 }
