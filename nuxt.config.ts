ab4f719aad49113eff5534a926f0afa525d04c26
jackgraddon <hello@jackgraddon.com>
2026-01-19 09:19:35 -0800

    Add basic styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  css: [
+    '@/assets/styles/main.css',
+    '@/assets/styles/variables.css',
+    '@/assets/fonts/fonts.css',
+  ],
+
   modules: [
     '@nuxt/a11y',
     '@nuxt/content',
