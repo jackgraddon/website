7552a5abd04613315c7823d0663e947d863b7fca
jackgraddon <hello@jackgraddon.com>
2026-01-19 09:20:19 -0800

    Add meta

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,22 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: 'Jack Graddon',
+  htmlAttrs: {
+    lang: 'en',
+  },
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'description', content: '' }
+  ],
+})
+</script>
+
 <template>
   <div>
+    <Background />
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
