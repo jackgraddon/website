fb0321e7e3ef5c6d3f60c7b98c65a0dd34997409
jackgraddon <hello@jackgraddon.com>
2026-01-19 09:21:17 -0800

    Implement hero component, begin working on paged content component.

---
+++ app/components/pagedContent.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main class="paged-content">
+        <slot />
+    </main>
+</template>
+
+<style scoped>
+.paged-content {
+    max-width: var(--breakpoint-sm);
+    margin: 0 auto;
+    padding: 2rem 0;
+
+    display: grid;
+    gap: 4rem;
+
+    section {
+        height: 100vh;
+        scroll-margin-top: 20vh;
+
+        display: flex;
+        flex-direction: column;
+    }
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <Hero variant="landing" iconName="solar:hand-shake-line-duotone" title="Jack Graddon"/>
+    <PagedContent>
+      <section id="intro">
+        <h1>Hello!</h1>
+        <p>I'm Jack, a freelance Design Engineer, Web Developer, and Graphic Designer. I've worked on projects and designs for my local school, growing influencers, companies, and competitions. I've always desired to make technology more accessible by developing user interfaces that are simple to navigate and visually engaging.</p>
+      </section>
+      <section id="work">
+        <h2>My Work</h2>
+        <p>Feel free to explore my portfolio and see some of the projects I've worked on. I'm passionate about creating beautiful and functional designs that meet the needs of my clients.</p>
+      </section>
+    </PagedContent>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
