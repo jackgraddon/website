27d32fc287408a266ce6ff625c4b21edd3d34f95
jackgraddon <hello@jackgraddon.com>
2026-01-19 21:38:23 -0800

    add footer component

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -15,6 +15,7 @@
         <p>Feel free to explore my portfolio and see some of the projects I've worked on. I'm passionate about creating beautiful and functional designs that meet the needs of my clients.</p>
       </section>
     </PagedContent>
+    <Footer />
   </div>
 </template>
 
