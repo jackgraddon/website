83dcfc7577e11a284111f6af7d56182415a68b40
jackgraddon <hello@jackgraddon.com>
2026-01-19 21:36:41 -0800

    add basic footer

---
+++ app/components/footer.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="footer-content">
+      <h2>Footer Content</h2>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+    padding: 2rem 0;
+    text-align: center;
+    color: var(--color-text-light);
+}
+.footer-content {
+    max-width: var(--breakpoint-sm);
+    margin: 0 auto;
+}   
+</style>
