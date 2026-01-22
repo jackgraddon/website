3900ca5c07db0f265420caf7f74ab231a6278cf6
jackgraddon <hello@jackgraddon.com>
2026-01-22 13:22:49 -0800

    Add card element

---
+++ app/components/card.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import Background from './background.vue';
+
+const props = defineProps<{
+    image?: string
+}>();
+</script>
+
+<template>
+    <div class="card" :style="{backgroundImage: props.image ? 'url(' + props.image + ')' : 'none'}">
+        <slot class="card-header" name="header" />
+        <slot />
+        <slot class="card-footer" name="footer" />
+    </div>
+</template>
+
+<style scoped>
+.card {
+    width: 100%;
+    aspect-ratio: 1.6;
+    position: relative;
+    padding: 0.5rem;
+
+    background-color: var(--color-bg-surface);
+    background-position: center;
+    background-size: cover;
+    backdrop-filter: blur(7pt);
+    
+    border: 1px solid var(--color-border);
+    border-radius: 32pt;
+
+    display: flex;
+    flex-direction: column;
+
+    overflow: hidden;
+}
+</style>
