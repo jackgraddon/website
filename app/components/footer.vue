4480ef483625525ecccf3e33610a14967d397ce7
jackgraddon <hello@jackgraddon.com>
2026-01-22 20:25:13 -0800

    Change verbing padding and structure of footer

--- app/components/footer.vue
+++ app/components/footer.vue
@@ -4,20 +4,15 @@
 
 <template>
   <footer class="footer">
-    <div class="footer-content">
       <h2>Footer Content</h2>
-    </div>
   </footer>
 </template>
 
 <style scoped>
 .footer {
-    padding: 2rem 0;
-    text-align: center;
+    max-width: var(--breakpoint-lg);
+    padding: 2rem 1rem;
+    margin-inline: auto;
     color: var(--color-text-light);
-}
-.footer-content {
-    max-width: var(--breakpoint-sm);
-    margin: 0 auto;
 }   
 </style>
